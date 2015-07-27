e07e31d599b668910095ddad3f87c23600a4e88b
David Ernst <git@dsernst.com>
2015-07-27 00:11:05 -0700

    add in audio info object with paths and durations

--- src/pages/HomePage/PlaybackScreen.js
+++ src/pages/HomePage/PlaybackScreen.js
@@ -94,3 +94,27 @@ module.exports = React.createClass({
     this.props.switchScreens({onConfigScreen: true}, {});
   }
 });
+
+var audio = {
+  directory: '../audio/',
+  closingChanting: {
+    filename: 'closing-chanting.mp3',
+    length: ((2 * 60) + 53) * 1000
+  },
+  closingMetta: {
+    filename: 'closing-metta.mp3',
+    length: 46 * 1000
+  },
+  introChanting: {
+    filename: 'intro-chanting.mp3',
+    length: ((2 * 60) + 13) * 1000
+  },
+  introInstructions: {
+    filename: 'intro-instructions.mp3',
+    length: 16 * 1000
+  },
+  mettaIntro: {
+    filename: 'metta-intro.mp3',
+    length: 14 * 1000
+  }
+};
