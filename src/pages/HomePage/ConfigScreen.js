8187183ecbf81c64bcaf97e0ffac624fae1b1f23
David Ernst <git@dsernst.com>
2015-09-05 14:49:55 -0700

    Lint main js files

--- src/pages/HomePage/ConfigScreen.js
+++ src/pages/HomePage/ConfigScreen.js
@@ -1,27 +1,27 @@
-var React = require('react');
-var mui = require('material-ui');
-var Toggle = mui.Toggle;
-var RaisedButton = mui.RaisedButton;
-var Colors = mui.Styles.Colors;
-var TimePicker = require('react-time-picker');
+var React = require('react')
+var mui = require('material-ui')
+var Toggle = mui.Toggle
+var RaisedButton = mui.RaisedButton
+var Colors = mui.Styles.Colors
+var TimePicker = require('react-time-picker')
 
 function durationStringToMilliseconds(string) {
   return string.split(':').reduce(function (memo, value, index) {
-    var minutes;
+    var minutes
     if (index === 0) {
-      minutes = value * 60;
+      minutes = value * 60
     } else if (index === 1) {
-      minutes = value;
+      minutes = value
     }
-    return memo + (minutes * 60 * 1000);
-  }, 0);
+    return memo + (minutes * 60 * 1000)
+  }, 0)
 }
 
 var DurationSelector = React.createClass({
   getInitialState: function () {
     return {
-      value: this.props.defaultDuration
-    };
+      value: this.props.defaultDuration,
+    }
   },
 
   render: function () {
@@ -30,14 +30,14 @@ var DurationSelector = React.createClass({
         <label>How long would you like to sit?</label>
         <TimePicker value={this.state.value} onChange={this.adjustDuration} style={{border: 'none', float: 'right', width: '300px', position: 'relative', top: '-10px'}}/>
       </div>
-    );
+    )
   },
 
   adjustDuration: function (string, moment) {
-    this.setState({value: string});
-    this.props.changeDuration(string);
-  }
-});
+    this.setState({value: string})
+    this.props.changeDuration(string)
+  },
+})
 
 module.exports = React.createClass({
   getInitialState: function () {
@@ -45,8 +45,8 @@ module.exports = React.createClass({
       duration: durationStringToMilliseconds(this.props.defaultDuration),
       introChanting: false,
       closingChanting: false,
-      metta: false
-    };
+      metta: false,
+    }
   },
 
   render: function () {
@@ -56,34 +56,34 @@ module.exports = React.createClass({
         <Toggle label="Include intro chanting? (2 min)" onToggle={this.toggleIntroChanting} />
         <Toggle label="Include closing chanting? (3 min)" onToggle={this.toggleClosingChanting} />
         <Toggle label="Include extended metta time? (4.5 min)" onToggle={this.toggleMetta} />
-        <RaisedButton label="Start" fullWidth={true} style={{margin: "20px 0"}} backgroundColor={Colors.lightGreen700} onClick={this.pressStart} />
+        <RaisedButton label="Start" fullWidth={true} style={{margin: '20px 0'}} backgroundColor={Colors.lightGreen700} onClick={this.pressStart} />
       </div>
-    );
+    )
   },
 
   changeDuration: function (string) {
-    this.setState({duration: durationStringToMilliseconds(string)});
-    this.props.updateDurationString(string);
-    // console.log('changed duration:', string);
+    this.setState({duration: durationStringToMilliseconds(string)})
+    this.props.updateDurationString(string)
+    // console.log('changed duration:', string)
   },
 
   toggleIntroChanting: function (e, toggled) {
-    this.setState({introChanting: toggled});
-    // console.log('toggled introChanting:', toggled);
+    this.setState({introChanting: toggled})
+    // console.log('toggled introChanting:', toggled)
   },
 
   toggleClosingChanting: function (e, toggled) {
-    this.setState({closingChanting: toggled});
-    // console.log('toggled closingChanting:', toggled);
+    this.setState({closingChanting: toggled})
+    // console.log('toggled closingChanting:', toggled)
   },
 
   toggleMetta: function (e, toggled) {
-    this.setState({metta: toggled});
-    // console.log('toggled metta:', toggled);
+    this.setState({metta: toggled})
+    // console.log('toggled metta:', toggled)
   },
 
   pressStart: function () {
-    // console.log('pressed start:', this.state);
-    this.props.switchScreens({onConfigScreen: false}, this.state);
-  }
-});
+    // console.log('pressed start:', this.state)
+    this.props.switchScreens({onConfigScreen: false}, this.state)
+  },
+})
