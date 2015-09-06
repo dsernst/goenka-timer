46631e1c6cf64d8511e29fd4d3d7dcf769ce6e5e
David Ernst <git@dsernst.com>
2015-09-05 21:27:39 -0700

    code cleanup

--- src/pages/HomePage/DurationSelector.jsx
+++ src/pages/HomePage/DurationSelector.jsx
@@ -18,7 +18,7 @@ module.exports = React.createClass({
     }
 
     return 0,
-      <div className="duration-setting" style={{textAlign: 'center'}}>
+      <div className="duration-config" style={{textAlign: 'center'}}>
         <h3 style={{textAlign: 'left'}}>How long would you like to sit?</h3>
         <TimePicker value={this.state.value} onChange={this.adjustDuration} style={{border: 'none', width: 300, position: 'relative', top: '-10px'}} hourInputStyle={inputStyle} minuteInputStyle={inputStyle} arrowStyle={{color: '#a6a6a6', fontSize: 36}} arrowOverStyle={{color: '#f6f6f6', background: 'none', transition: 'color .1s ease-in-out'}}/>
       </div>
